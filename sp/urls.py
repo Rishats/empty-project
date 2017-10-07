7f9342317d75082fb48a9a36ef8a91c6038c989b
Rishats <sultanov-rishat@inbox.ru>
2017-10-08 02:13:08 +0600

    Add virtual console demo - without backend.

--- sp/urls.py
+++ sp/urls.py
@@ -5,4 +5,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name="users/main.html")),
+    url(r'^console/', TemplateView.as_view(template_name="users/console.html")),
 ]
