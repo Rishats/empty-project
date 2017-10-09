08b9f4b3fda8dd126c96696517aea95f8c40b1ca
Rishats <sultanov-rishat@inbox.ru>
2017-10-09 23:29:32 +0600

    Add jet style - admin. Rename admin name. And create requirements.txt

--- sp/urls.py
+++ sp/urls.py
@@ -1,9 +1,14 @@
+from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
+
+
+admin.site.site_header = 'Learn SQL'
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name="users/main.html")),
     url(r'^console/', TemplateView.as_view(template_name="users/console.html")),
+    url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
 ]
