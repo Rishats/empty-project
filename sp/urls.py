052011d5d0a074204e37a11baafe729cd31366cd
Rishats <sultanov-rishat@inbox.ru>
2017-10-10 00:12:16 +0600

    Fix admin header

--- sp/urls.py
+++ sp/urls.py
@@ -4,11 +4,13 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 
-admin.site.site_header = 'Learn SQL'
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name="users/main.html")),
     url(r'^console/', TemplateView.as_view(template_name="users/console.html")),
     url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
 ]
+
+# Change admin site title
+admin.site.site_header = ("Students Platform SQL")
+admin.site.site_title = ("ST Platform")
