14aa7940aa37955d3eab521b96daff265d118f32
Rishats <sultanov-rishat@inbox.ru>
2017-10-09 23:39:17 +0600

    Fix some problem with model Student.

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Students(models.Model):
+class Student(models.Model):
     lname = models.CharField(max_length=256)
     fname = models.CharField(max_length=256)
     slug = models.CharField(max_length=256)
