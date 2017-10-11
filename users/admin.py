50bbf5ad231d9ade9f669848bf07ebe5c7c0309a
Rishats <sultanov-rishat@inbox.ru>
2017-10-12 00:56:26 +0600

    Fix migration and add Work for Students.

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
 from .models import Student
+from .models import Work
 
 admin.site.register(Student)
+admin.site.register(Work)
--- users/models.py
+++ users/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
-
 class Student(models.Model):
-    lname = models.CharField(max_length=256)
-    fname = models.CharField(max_length=256)
-    slug = models.CharField(max_length=256)
+    first_name = models.CharField(max_length=80, blank=True)
+    last_name = models.CharField(max_length=80, blank=True)
+    slug = models.CharField(max_length=255, blank=True)
+    created_at = models.DateTimeField('Datetime created')
+
+class Work(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    name = models.CharField(max_length=80, blank=True)
+    description = models.CharField(max_length=255, blank=True)
+    points = models.PositiveSmallIntegerField(blank=True)
     created_at = models.DateTimeField('Datetime created')
 
