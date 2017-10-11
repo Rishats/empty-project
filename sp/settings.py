5d354510d08fd5ae8fcc383745047725fb270586
Rishats <sultanov-rishat@inbox.ru>
2017-10-11 14:37:37 +0600

    Add registration with Email confirm.

--- sp/settings.py
+++ sp/settings.py
@@ -125,3 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'rihasultanov@gmail.com'
+EMAIL_HOST_PASSWORD = 'secret'
+EMAIL_PORT = 587
--- sp/urls.py
+++ sp/urls.py
@@ -2,13 +2,16 @@ from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
-
+from users import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name="users/main.html")),
     url(r'^console/', TemplateView.as_view(template_name="users/console.html")),
     url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
 ]
 
 # Change admin site title
