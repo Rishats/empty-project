02348c4231819f49c7d49d2f1855aa601ac45d6b
Rishats <sultanov-rishat@inbox.ru>
2017-10-14 00:19:55 +0600

    Fix Some old relationships users-students

--- sp/urls.py
+++ sp/urls.py
@@ -6,8 +6,8 @@ from students import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name="users/main.html")),
-    url(r'^console/', TemplateView.as_view(template_name="users/console.html")),
+    url(r'^$', TemplateView.as_view(template_name="students/main.html")),
+    url(r'^console/', TemplateView.as_view(template_name="students/console.html")),
     url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
