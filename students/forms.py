264cea49df943b14dc8cf77ce998108645f9d33b
Rishats <sultanov-rishat@inbox.ru>
2017-10-18 15:35:55 +0600

    Add validate email address

--- students/forms.py
+++ students/forms.py
@@ -1,11 +1,27 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class SignupForm(UserCreationForm):
-    email = forms.EmailField(max_length=200, help_text='Required')
+    email = forms.EmailField(required=True)
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        domain = data.split('@')[1]
+        domain_list = ["iuca.kg", ]
+        if domain not in domain_list:
+            raise ValidationError["Please enter an Email Address with a valid domain"]
+        return data
+
+    def save(self, commit=True):
+        user = super(SignupForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
